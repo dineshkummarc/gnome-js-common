aa40e5b0bdb845a99ea51864d0537f76faa3d450
Javier Jardón <jjardon@gnome.org>
2010-04-01 02:51:23 +0200

    Modernize autotools configuration
    New requirements:
    automake >= 1.10
    libtool >= 2.2

--- autogen.sh
+++ autogen.sh
@@ -13,11 +13,9 @@ PKG_NAME="gnome-js-common"
 }
 
 which gnome-autogen.sh || {
-    echo "You need to install gnome-common from GNOME SVN and make"
+    echo "You need to install gnome-common and make"
     echo "sure the gnome-autogen.sh script is in your \$PATH."
     exit 1
 }
 
-REQUIRED_AUTOMAKE_VERSION=1.9 
-REQUIRED_INTLTOOL_VERSION=0.40.4
 . gnome-autogen.sh
